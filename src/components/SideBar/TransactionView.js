0ddf026e6a714d358233579ea64b47b031054cb4
Gavin Lu <gavinlu94@gmail.com>
2023-08-11 20:15:18 -0400

    PB-23: Add Filters to Transaction Tab
        Add filters for date, amount, and description to let users search through
        their transaction history.

--- src/components/SideBar/TransactionView.js
+++ src/components/SideBar/TransactionView.js
@@ -1,6 +1,8 @@
-import { Table } from "react-bootstrap";
 import { useContext } from "react";
+import { Table, Form } from "react-bootstrap";
 import { DataContext } from "../../contexts/DataContext";
+import { SideBarContext } from "../../contexts/SideBarContext";
+import { filterTransactions } from "../../events/SideBarEvents";
 import { FormatMoney, FormatDate } from "../../utils/Utils";
 
 
@@ -15,11 +17,62 @@ const TransactionRow = ({ transaction }) => {
     );
 }
 
+const FilterTransactionsForm = () => {
+
+    const { dispatch } = useContext(SideBarContext);
+
+    const handleOnDataChange = (event) => {
+
+        const dispatchType = event.target.getAttribute('dispatchType');
+        let value = event.target.value;
+
+        if (['SET_MIN_DATE', 'SET_MAX_DATE'].includes(dispatchType)) {
+            value = value != '' ? new Date(value) : value;
+        } else if (['SET_MIN_AMOUNT', 'SET_MAX_AMOUNT'].includes(dispatchType)) {
+            value = value != '' ? parseFloat(value) : value;
+        }
+
+        dispatch({
+            type: dispatchType,
+            payload: {
+                data: value,
+            }
+        });
+
+    }
+
+    return (
+        <Form>
+            <Table>
+                <tr>
+                    <td>Date</td>
+                    <td>From: <Form.Control type="date" dispatchtype="SET_MIN_DATE" onChange={handleOnDataChange} /></td>
+                    <td>To: <Form.Control type="date" dispatchtype="SET_MAX_DATE" onChange={handleOnDataChange} /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td colspan="2"><Form.Control type="text" dispatchtype="SET_DESCRIPTION" onChange={handleOnDataChange} placeholder="Search" /></td>
+                </tr>
+                <tr>
+                    <td>Amount</td>
+                    <td>Min: <Form.Control type="number" dispatchtype="SET_MIN_AMOUNT" onChange={handleOnDataChange} /></td>
+                    <td>Max: <Form.Control type="number" dispatchtype="SET_MAX_AMOUNT" onChange={handleOnDataChange} /></td>
+                </tr>
+            </Table>
+        </Form>
+    );
+}
+
 
 const TransactionTable = () => {
 
-    let { state } = useContext(DataContext);
-    let transactions = 'transactions' in state ? state.transactions : [];
+    const { state: sideBarState } = useContext(SideBarContext);
+    const { minDate, maxDate, description, minAmount, maxAmount } = sideBarState;
+
+    const { state: dataState } = useContext(DataContext);
+
+    let transactions = 'transactions' in dataState ? dataState.transactions : [];
+    transactions = filterTransactions(transactions, minDate, maxDate, description, minAmount, maxAmount);
 
     return (
         <Table striped bordered hover>
@@ -43,7 +96,11 @@ const TransactionTable = () => {
 
 const TransactionView = () => {
     return (
-        <TransactionTable />
+        <div>
+            <h1>Transactions</h1>
+            <FilterTransactionsForm />
+            <TransactionTable />
+        </div>
     );
 }
 
