ed1ae1fabe1751da43767d9c9dd3f61b5e8d8419
Gavin Lu <gavinlu94@gmail.com>
2023-08-14 23:45:06 -0400

    PB-48: Sort Displayed Tables
        Enable table sorting for the transaction and description tables by creating
        a consolidated SortableTable component that can be used by both tables and
        houses all the required logic for sorting and displaying the tables.

--- src/components/SideBar/TransactionView.js
+++ src/components/SideBar/TransactionView.js
@@ -1,73 +1,15 @@
 import { useContext } from "react";
-import { Table, Form, Button } from "react-bootstrap";
-import { FilterForm } from "./Utils";
-import { ACTION_TOGGLE_OBJECT_VISIBILITY, DataContext } from "../../contexts/DataContext";
-import { SideBarContext, ACTION_SET_MIN_DATE, ACTION_SET_MAX_DATE, ACTION_SET_DESCRIPTION, ACTION_SET_MIN_AMOUNT, ACTION_SET_MAX_AMOUNT, ACTION_SET_TAG } from "../../contexts/SideBarContext";
-import { filterTransactions } from "../../events/SideBarEvents";
-import { FormatMoney, FormatDate } from "../../utils/Utils";
+import { Button } from "react-bootstrap";
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+import { FilterForm, SortableTable } from "./Utils";
+import { ACTION_TOGGLE_OBJECT_VISIBILITY, DataContext } from "../../contexts/DataContext";
+import { ACTION_SET_DESCRIPTION, ACTION_SET_MAX_AMOUNT, ACTION_SET_MAX_DATE, ACTION_SET_MIN_AMOUNT, ACTION_SET_MIN_DATE, ACTION_SET_TAG, SideBarContext } from "../../contexts/SideBarContext";
+import { filterTransactions } from "../../events/SideBarEvents";
+import { FormatDate, FormatMoney } from "../../utils/Utils";
 
 
-const TransactionRow = ({ transaction }) => {
-
-    const { dispatch } = useContext(DataContext);
-    const handleActiveButtonClick = (event) => {
-        dispatch({
-            type: ACTION_TOGGLE_OBJECT_VISIBILITY,
-            payload: {
-                object: transaction,
-                isActive: !transaction.isActive
-            }
-        });
-    }
-
-    return (
-        <tr>
-            <td>{FormatDate(transaction.date)}</td>
-            <td>{transaction.description.name}</td>
-            <td>{FormatMoney(transaction.amount)}</td>
-            <td>{transaction.transactionType}</td>
-            <td>{transaction.description.tag.name}</td>
-            <td><Button><FontAwesomeIcon onClick={handleActiveButtonClick} icon={transaction.isActive ? faPlay : faPause} /></Button></td>
-        </tr>
-    );
-}
-
-
-const TransactionTable = () => {
-
-    const { state: sideBarState } = useContext(SideBarContext);
-    const { minDate, maxDate, description, minAmount, maxAmount, tag } = sideBarState;
-
-    const { state: dataState } = useContext(DataContext);
-
-    let transactions = 'transactions' in dataState ? dataState.transactions : [];
-    transactions = filterTransactions(transactions, minDate, maxDate, description, minAmount, maxAmount, tag);
-
-    return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Description</th>
-                    <th>Amount</th>
-                    <th>Transaction Type</th>
-                    <th>Tag</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {transactions.map((transaction) => {
-                    return <TransactionRow key={transaction.id} transaction={transaction} />
-                })}
-            </tbody>
-        </Table>
-    )
-}
-
-
-const TransactionView = () => {
+const TransactionFilterForm = () => {
 
     const { dispatch } = useContext(SideBarContext);
 
@@ -81,15 +23,78 @@ const TransactionView = () => {
     const dateFields = ['Date'];
     const floatFields = ['Amount'];
 
+    return (
+        <FilterForm dispatch={dispatch}
+            fieldNames={filterFieldNames}
+            dispatchTypes={dispatchTypes}
+            dateFields={dateFields}
+            floatFields={floatFields} />
+    )
+}
+
+
+const TransactionTable = () => {
+
+    const { state, dispatch } = useContext(DataContext);
+
+    const handleActiveButtonClick = (transaction) => {
+        dispatch({
+            type: ACTION_TOGGLE_OBJECT_VISIBILITY,
+            payload: {
+                object: transaction,
+                isActive: !transaction.isActive
+            }
+        });
+    }
+    const schema = [{
+        name: 'Date',
+        property: 'date',
+        getProperty: (transaction) => transaction.date,
+        displayField: (transaction) => { return FormatDate(transaction.date) },
+        sort: true,
+    }, {
+        name: 'Description',
+        property: 'description.name',
+        getProperty: (transaction) => transaction.description.name,
+        sort: true
+    }, {
+        name: 'Amount',
+        property: 'amount',
+        getProperty: (transaction) => { return transaction.amount },
+        displayField: (transaction) => { return FormatMoney(transaction.amount) },
+        sort: true,
+    }, {
+        name: 'Transaction Type',
+        property: 'transactionType',
+        getProperty: (transaction) => transaction.transactionType,
+        sort: true,
+    }, {
+        name: 'Tag',
+        property: 'description.tag.name',
+        getProperty: (transaction) => transaction.description.tag.name,
+        sort: true
+    }, {
+        name: 'Active',
+        displayField: (transaction) => { return <Button><FontAwesomeIcon onClick={() => handleActiveButtonClick(transaction)} icon={transaction.isActive ? faPlay : faPause} /></Button> },
+        sort: false,
+    }];
+    let transactions = 'transactions' in state ? state.transactions : [];
+
+    const { state: sideBarState } = useContext(SideBarContext);
+    const { minDate, maxDate, description, minAmount, maxAmount, tag } = sideBarState;
+
+    return (
+        <SortableTable schema={schema} data={transactions} filter={(transactions) => filterTransactions(transactions, minDate, maxDate, description, minAmount, maxAmount, tag)}/>
+    )
+}
+
+
+const TransactionView = () => {
 
     return (
         <div>
             <h1>Transactions</h1>
-            <FilterForm dispatch={dispatch}
-                fieldNames={filterFieldNames}
-                dispatchTypes={dispatchTypes}
-                dateFields={dateFields}
-                floatFields={floatFields} />
+            <TransactionFilterForm />
             <TransactionTable />
         </div>
     );
